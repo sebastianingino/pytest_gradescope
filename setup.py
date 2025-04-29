1802e6ee835fbd9b149f38612dbc5e6e0719d12b
Sebastian Ingino <ingino@stanford.edu>
2025-04-28 22:36:01 -0700

    Updates

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pytest_gradescope",
+    name="pytest-gradescope",
+    version="0.1.0",
+    description="A pytest plugin for Gradescope integration",
+    readme="README.md",
+
+    author="Sebastian Ingino",
+    author_email="sebastian@ingino.me",
+    license="MIT",
+
     packages=find_packages(exclude=["contrib", "docs", "tests"]),
     # the following makes a plugin available to pytest
     entry_points={
         "pytest11": [
-            "pytest_gradescope = pytest_gradescope.pytest_plugin",
+            "pytest-gradescope = pytest_gradescope.pytest_plugin",
         ]
     },
     # custom PyPI classifier for pytest plugins
